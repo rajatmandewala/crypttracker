eefcc613dd005f0fa5762c96d6dfdfcc0ccd0031
rajatmandewala <rajatmandewala@gmail.com>
2019-12-25 09:56:50 +0530

    nse module done

---
+++ src/main/java/com/rajat/cryp/controller/NSEController.java
@@ -0,0 +1,24 @@
+package com.rajat.cryp.controller;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rajat.cryp.service.NSEService;
+
+@RestController
+@RequestMapping(value="/nse")
+public class NSEController {
+	
+	@Autowired
+	private NSEService nseService;
+	
+	@GetMapping
+	public String getFile() throws IOException {
+		return nseService.downloadNseFile();
+	}
+
+}
